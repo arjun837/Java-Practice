40ec87dd23de8a25c18c691e26a3100e1d0626a7
arjun837 <arjunparikh111@gmail.com>
2025-02-01 20:11:21 +0530

    while loop

---
+++ practiceautomation05012025/src/day4/whileandloops.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class whileandloops {
+		public static void main (String[] args)
+		// Example of while loop for printing 1 to 10
+		{
+			int i=1;
+			while(i<=10)
+			{
+			System.out.println(i);
+			i++;
+			}
+		}
+	
+
+}
