7b2ed6b01ee89fa995d7ca3e10b004fbb6da2fc7
arjun837 <arjunparikh111@gmail.com>
2025-02-01 20:20:02 +0530

    example 3 while loop

--- practiceautomation05012025/src/day4/whileandloops.java
+++ practiceautomation05012025/src/day4/whileandloops.java
@@ -1,16 +1,40 @@
 package day4;
 
 public class whileandloops {
-		public static void main (String[] args)
-		// Example of while loop for printing 1 to 10
+	public static void main (String[] args)
+//		// Example of while loop for printing 1 to 10
+//		{
+//			int i=1;
+//			while(i<=10)
+//			{
+//			System.out.println(i);
+//			i++;
+//			}
+//		}
+//	
+//
+//}
+	
+//	-------------example 2 print hello 10 times
+//	{
+//		int i=1;
+//		while (i<=10)
+//		{
+//			System.out.println("hello");
+//			i++;
+//		}
+//	}
+//}
+//------- Example 3 print even number in 1 to 10
+	{
+		int i=2;
 		{
-			int i=1;
-			while(i<=10)
+			while (i<=10)
 			{
-			System.out.println(i);
-			i++;
+				System.out.println(i);
+				//i=i+2;
+				i+=2;
 			}
 		}
-	
-
+	}
 }
