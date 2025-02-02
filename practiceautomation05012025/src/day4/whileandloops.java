6a0e8a73024eea4f6bb025a6592ebc3d40c88efc
arjun837 <arjunparikh111@gmail.com>
2025-02-02 18:59:05 +0530

    1 to 10 all odd or even print

--- practiceautomation05012025/src/day4/whileandloops.java
+++ practiceautomation05012025/src/day4/whileandloops.java
@@ -26,15 +26,34 @@ public class whileandloops {
 //	}
 //}
 //------- Example 3 print even number in 1 to 10
+//	{
+//		int i=2;
+//		{
+//			while (i<=10)
+//			{
+//				System.out.println(i + "Even");
+//				//i=i+2;
+//				i+=2;
+//			}
+//		}
+//	}
+//}
+
+//----------Example 5 print odd and even both in 1 to 10
 	{
-		int i=2;
+		int i=1;
+		while(i<=10)
 		{
-			while (i<=10)
-			{
-				System.out.println(i);
-				//i=i+2;
-				i+=2;
-			}
+			if(i%2==1)
+		{
+		System.out.println(i + "Odd");
+		}
+		else
+		{
+		System.out.println(i + "Even");
+		}
+		i++;
+			
 		}
 	}
 }
