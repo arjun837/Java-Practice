6b872bdad8b806801d5a5568518a692b98806c41
arjun837 <arjunparikh111@gmail.com>
2025-05-25 13:16:15 +0530

    Practicing the whileVSDoWhile loops and understanding how it works with a example

--- practiceautomation05012025/src/day5/dowhileloop.java
+++ practiceautomation05012025/src/day5/dowhileloop.java
@@ -1,15 +1,27 @@
 package day5;
 
-public class dowhileloop {
+//public class dowhileloop {
+//	public static void main(String[] args) {
+//		int i=1;
+//		do {
+//			System.out.println(i);
+//			i++;
+//			
+//			
+//		}while(i<=10);
+//		
+//	}
+//}
+// started practicing after almost 3 months and above example is of do while loop using it I have print numbers from 1 to 10.
+
+
+public class dowhileloop{
 	public static void main(String[] args) {
-		int i=1;
+		int i=10;
 		do {
 			System.out.println(i);
-			i++;
-			
-			
-		}while(i<=10);
-		
+			i--;
+		} while (i>=1);
+		}
 	}
-}
-// started practicing after almost 3 months and above example is of do while loop using it I have print numbers from 1 to 10.
+
---
+++ practiceautomation05012025/src/day5/whilevsdowhile.java
@@ -0,0 +1,21 @@
+package day5;
+
+public class whilevsdowhile {
+	public static void main(String[] args) {
+//		int i=10;
+//		while(i<=5) // No output will be shown as the condition is not matching so in while loop initial number is not printed but in do while the first number is printed
+//			{
+//				
+//				System.out.println(i);
+//				i++;
+				
+		
+		int i=10;
+		do {
+			System.out.println(i);//here in do while the condition is executed and printed first value but the condition is not matching so it breaks the program.
+			i++;
+			
+			}while(i<=5);
+			}
+	}
+
